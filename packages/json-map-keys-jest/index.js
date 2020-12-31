c848a98d5dba98c8a9387173e2167db8f40fa394
Tiago Porto <tiagoporto@ymail.com>
2020-12-30 21:40:46 -0300

    Add jest package

---
+++ packages/json-map-keys-jest/index.js
@@ -0,0 +1,32 @@
+const jsonParser = require("json-map-keys-parser");
+const { interpolateName } = require("loader-utils");
+
+const createTransformer = (userOptions) => {
+  return {
+    // getCacheKey(sourceText, sourcePath, transformOptions, options) {
+    //   console.log("1 ", transformOptions);
+    //   return sourceText;
+    // },
+
+    process(sourceText, sourcePath) {
+      let prefix = "";
+      if (userOptions && userOptions.prefix) {
+        prefix = interpolateName(
+          { resourcePath: sourcePath },
+          userOptions.prefix,
+          {
+            sourceText,
+          }
+        );
+      }
+      const resources = jsonParser(JSON.parse(sourceText), prefix);
+
+      return JSON.stringify(resources);
+    },
+  };
+};
+
+module.exports = {
+  ...createTransformer(),
+  createTransformer,
+};
