4d3696a36a69570e6f3cd7e21b11a4567267afac
Tiago Porto <tiago@tiagoporto.com>
2025-10-04 01:06:12 -0300

    test: add junit reporter

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,7 +1,10 @@
+import { env } from 'node:process'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     include: ['./packages/**/*.test.{ts,tsx}'],
+    reporters: env.CI ? 'junit' : 'default',
+    outputFile: 'test-report.junit.xml',
   },
 })
