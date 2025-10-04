6c0e2096a88f9c5aa503d478c194a1c21f95e217
Tiago Porto <tiago@tiagoporto.com>
2025-10-04 20:41:19 -0300

    ci: setup semantic release

---
+++ .releaserc.mjs
@@ -0,0 +1,32 @@
+import { env } from 'node:process'
+
+const config = {
+  extends: 'semantic-release-monorepo',
+  branches: [
+    'main',
+    {
+      name: 'alpha',
+      channel: 'alpha',
+      prerelease: true,
+    },
+    {
+      name: 'beta',
+      channel: 'beta',
+      prerelease: true,
+    },
+  ],
+  preset: 'conventionalcommits',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    // '@sebbo2002/semantic-release-jsr',
+    '@anolilab/semantic-release-pnpm',
+  ],
+}
+
+// NOTE: add BRANCH env in the workflow
+if (env.BRANCH === 'main') {
+  config.plugins.push('@semantic-release/github')
+}
+
+export default config
